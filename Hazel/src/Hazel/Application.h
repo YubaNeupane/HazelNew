020760903f5f77ae526b07ce5f1e1e13862a2014
Yuba Neupane <neopaneyb2@gmail.com>
2025-12-15 00:11:06 -0500

    Window Events

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Core.h"
-#include "Events/Event.h"
+#include "Hazel/Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace Hazel {
@@ -11,7 +11,12 @@ namespace Hazel {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
+
 	private:
+		bool OnWindowClosed(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
