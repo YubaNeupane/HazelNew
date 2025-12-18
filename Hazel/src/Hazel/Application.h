72275761589c8d67676ac5c9d2ea94f90d0d5060
Yuba Neupane <neopaneyb2@gmail.com>
2025-12-18 16:02:26 -0500

    Our First Triangle!

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -31,6 +31,8 @@ namespace Hazel {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
+		unsigned int m_VertexArrayID, m_VertexBufferID, m_IndexBufferID;
+
 	private:
 		static Application* s_Instance;
 	};
